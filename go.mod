405a01bd64cd12d6d85ca69b1f849edc46bb9568
ImVexed <icemanthedtior@gmail.com>
2020-11-21 15:39:13 -0800

    swap to fastrand + worker pool

---
+++ go.mod
@@ -0,0 +1,9 @@
+module fractal
+
+go 1.15
+
+require (
+	github.com/valyala/fastrand v1.0.0 // indirect
+	gitlab.com/NebulousLabs/fastrand v0.0.0-20181126182046-603482d69e40 // indirect
+	golang.org/x/crypto v0.0.0-20201117144127-c1f2f97bffc9 // indirect
+)
--- main.go
+++ main.go
@@ -6,10 +6,11 @@ import (
 	"image/color"
 	"image/png"
 	"log"
-	"math/rand"
 	"os"
-	"sync"
+	"runtime"
 	"time"
+
+	"github.com/valyala/fastrand"
 )
 
 const (
@@ -22,16 +23,14 @@ const (
 	//size = 2.5
 
 	// Quality
-	imgWidth = 1024
+	imgWidth = 1024 * 10
 	maxIter  = 1000
 	samples  = 50
-
-	showProgress = true
 )
 
 func main() {
 	log.Println("Allocating image...")
-	img := image.NewRGBA(image.Rect(0, 0, imgWidth, imgWidth))
+	img := image.NewRGBA(image.Rect(0, 0, int(imgWidth), int(imgWidth)))
 
 	log.Println("Rendering...")
 	start := time.Now()
@@ -52,93 +51,70 @@ func main() {
 	log.Println("Done!")
 }
 
+func fastRand() float64 {
+	return float64(int64(fastrand.Uint32())+int64(fastrand.Uint32())) / (1 << 63)
+}
 func render(img *image.RGBA) {
-	progress := make(chan struct{})
+	jobs := make(chan int)
 
-	// Progress
-	if showProgress {
-		go func() {
-			for i := 1; ; i++ {
-				if _, k := <- progress; !k { break }
-				fmt.Printf("\r%d/%d (%d%%)", i, imgWidth, int(100*(float64(i) / float64(imgWidth))))
-			}
-			fmt.Println()
-		}()
+	for w := 1; w <= runtime.NumCPU(); w++ {
+		go worker(img, jobs)
 	}
 
-	var wg sync.WaitGroup
 	for y := 0; y < imgWidth; y++ {
-		wg.Add(1)
-		go func(y int) {
-			for x := 0; x < imgWidth; x++ {
-				var sampledColours [samples]color.RGBA
-				for i := 0; i < samples; i++ {
-					nx := size * ((float64(x) + rand.Float64()) / float64(imgWidth)) + px
-					ny := size * ((float64(y) + rand.Float64()) / float64(imgWidth)) + py
-					sampledColours[i] = paint(mandelbrotIter(nx, ny, maxIter))
-				}
-				var r, g, b int
-				for _, colour := range sampledColours {
-					r += int(colour.R)
-					g += int(colour.G)
-					b += int(colour.B)
-				}
-				img.SetRGBA(x, y, color.RGBA{
-					R: uint8(float64(r) / float64(samples)),
-					G: uint8(float64(g) / float64(samples)),
-					B: uint8(float64(b) / float64(samples)),
-					A: 255,
-				})
-			}
-			if showProgress {
-				progress <- struct{}{}
-			}
-			wg.Done()
-		}(y)
+		jobs <- y
+		fmt.Printf("\r%d/%d (%d%%)", y, imgWidth, int(100*(float64(y)/imgWidth)))
 	}
-	wg.Wait()
-	close(progress)
+	fmt.Println()
+}
+
+func worker(img *image.RGBA, jobs <-chan int) {
+	for y := range jobs {
+		for x := 0; x < imgWidth; x++ {
+			var r, g, b int
+
+			for i := 0; i < samples; i++ {
+				nx := size*((float64(x)+fastRand())/float64(imgWidth)) + px
+				ny := size*((float64(y)+fastRand())/float64(imgWidth)) + py
+				colour := paint(mandelbrotIter(nx, ny, maxIter))
+
+				r += int(colour.R)
+				g += int(colour.G)
+				b += int(colour.B)
+			}
+
+			img.SetRGBA(int(x), int(y), color.RGBA{
+				R: uint8(float64(r) / float64(samples)),
+				G: uint8(float64(g) / float64(samples)),
+				B: uint8(float64(b) / float64(samples)),
+				A: 255,
+			})
+		}
+	}
 }
 
 func paint(r float64, n int) color.RGBA {
-	var insideSet = color.RGBA{ R: 255, G: 255, B: 255, A: 255 }
+	var insideSet = color.RGBA{R: 255, G: 255, B: 255, A: 255}
 
 	if r > 4 {
-		c := hslToRGB(float64(n) / 800 * r, 1, 0.5)
+		c := hslToRGB(float64(n)/800*r, 1, 0.5)
 		return c
-	} else {
-		return insideSet
 	}
+
+	return insideSet
 }
 
 func mandelbrotIter(px, py float64, maxIter int) (float64, int) {
 	var x, y, xx, yy, xy float64
 
 	for i := 0; i < maxIter; i++ {
-		xx, yy, xy = x * x, y * y, x * y
-		if xx + yy > 4 {
+		xx, yy, xy = x*x, y*y, x*y
+		if xx+yy > 4 {
 			return xx + yy, i
 		}
 		x = xx - yy + px
-		y = 2 * xy + py
+		y = 2*xy + py
 	}
 
 	return xx + yy, maxIter
 }
-
-// by u/Boraini
-//func mandelbrotIterComplex(px, py float64, maxIter int) (float64, int) {
-//	var current complex128
-//	pxpy := complex(px, py)
-//
-//	for i := 0; i < maxIter; i++ {
-//		magnitude := cmplx.Abs(current)
-//		if magnitude > 2 {
-//			return magnitude * magnitude, i
-//		}
-//		current = current * current + pxpy
-//	}
-//
-//	magnitude := cmplx.Abs(current)
-//	return magnitude * magnitude, maxIter
-//}
